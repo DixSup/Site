c8e2a6f3e202468d8b8413cc305c18e7b0156ef6
DixSun <26sashok09@mail.ru>
2023-04-07 14:00:09 +0300

    main

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,11 +2,15 @@ const header = document.querySelector(".header");
 const backButtonPhoto = document.querySelector('.backButton');
 const nextButtonPhoto = document.querySelector('.nextButton');
 
+const contact = document.querySelector(".contactsLink");
+const about = document.querySelector(".aboutLink");
+const photo = document.querySelector(".photoLink");
+
 let selectedPhoto = 1;
 let photoCount = 9;
 
 window.addEventListener('scroll',()=>{
-    if(this.scrollY > 1){
+    if(this.scrollY > 0.1){
         header.classList = "header header_fixed";
     }
     else{
@@ -29,3 +33,27 @@ function setPhoto(){
     let photo = document.querySelector(".photo1");
     photo.src = "./src/images/toPhotoList/" + selectedPhoto +".jpg";
 }
+contact.addEventListener('click', () =>{
+    let elem = document.querySelector(".contactsContainer");
+    const y = elem.getBoundingClientRect().top - elem.getBoundingClientRect().height;
+    window.scroll({
+    top: y,
+    behavior: 'smooth'
+    });
+});
+about.addEventListener('click', () =>{
+    let elem = document.querySelector(".aboutContainer");
+    const y = elem.getBoundingClientRect().top - elem.getBoundingClientRect().height / 3;;
+    window.scroll({
+    top: y,
+    behavior: 'smooth'
+    });
+});
+photo.addEventListener('click', () =>{
+    let elem = document.querySelector(".photoContainer");
+    const y = elem.getBoundingClientRect().top - elem.getBoundingClientRect().height / 3;;
+    window.scroll({
+    top: y,
+    behavior: 'smooth'
+    });
+});
